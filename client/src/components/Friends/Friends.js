38aecc7dd84f73389824b6b5ec28d37ac19b1dee
Ruel Alfonso Misa <ruel.g.misa@gmail.com>
2018-09-27 16:09:38 -0400

    Finished with UI for now

--- client/src/components/Friends/Friends.js
+++ client/src/components/Friends/Friends.js
@@ -2,17 +2,52 @@ import React, { Component } from 'react';
 
 import Button from '../UI/Button/Button';
 import ToolBar from '../UI/Toolbar/Toolbar';
+import InputField from '../UI/InputField/InputField';
+import styles from './Friends.css';
 class Friends extends Component {
 
     onClickHandler = () => {
         this.props.history.push('/user/:id/dashboard');
     }
 
+    onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        console.log('Looking for friends');
+    }
+
+    inputChangeHandler = (e) => {
+        console.log(e.target.value);
+    }
+
     render() {
         return (
             <React.Fragment>
                 <ToolBar />
-                <h1>FRIENDS HERE</h1>
+                <div className={ styles.Friends }>
+                    <h2>Friends</h2>
+                    <ul> 
+                        <li>Friend</li>
+                        <li>Friend</li>
+                        <li>Friend</li>
+                    </ul>
+                </div>
+                <div className={ styles.SearchFriends }>
+                    <h2>Add More Friends</h2>
+                    <form onSubmit={ this.onSubmitHandler }>
+                        <InputField 
+                        type="text"
+                        placeholder="Search for People"
+                        name="query"
+                        onChange={ this.inputChangeHandler }/>
+                        <Button
+                            btnType="Submit"
+                            btnSize="Medium">
+                            Search
+                        </Button>
+                    </form>
+                    
+                </div>
                 <Button 
                     onClick={ this.onClickHandler } 
                     btnType="Back"
